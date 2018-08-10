f2892a0e5f22027e1508ed8489d2d4c64d1f6b65
Khadivi <khadivi@huettinger.de>
2018-08-10 17:56:21 +0200

    add deadzone, add the functions for communication between Unity and IGP and add two GameManager and InputManager in the scene

--- paraglider unity/Assets/scripts/InputManager.cs
+++ paraglider unity/Assets/scripts/InputManager.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Devices;
-<<<<<<< HEAD
 using System;
 
 public class InputManager : MonoBehaviour {
@@ -17,23 +16,6 @@ public class InputManager : MonoBehaviour {
 
     float resultLeftRightMinus1To1 = 0;
     float resultUpDownMinus1To1 = 0;
-=======
-using System;
-
-public class InputManager : MonoBehaviour {
-    BmcmSensor sensor;
-
-    float rawVal_left       = 0;
-    float rawVal_right      = 0;
-    float rawVal_ripcord    = 0;
-
-    float normalizedVal_left = 0;
-    float normalizedVal_right = 0;
-    float normalizedVal_ripcord = 0;
-
-    float oldNormalizedVal_left = 0;
-    float oldNormalizedVal_right = 0;
->>>>>>> e4d8a318bd281d19399a63c807e2594eeae29315
 
     float minRawValue_leftgrip = 0;
     float maxRawValue_leftgrip = 5.14f;
@@ -44,16 +26,10 @@ public class InputManager : MonoBehaviour {
     float minRawVal_ripcord = 0;
     float maxRawVal_ripcord = 5.14f;
 
-<<<<<<< HEAD
     private float deadzone = 0;
     private float threshold_pull = 0;
     private float threshold_control = 0;
     private float threshold_idle = 0;
-=======
-    private float threshold_move = 0;
-    private float threshold_pull = 0;
-
->>>>>>> e4d8a318bd281d19399a63c807e2594eeae29315
     private bool showInputGUI = false;
     private bool enableKeyboard = false;
     private float timeout_idle = 30; // sec
@@ -67,7 +43,6 @@ public class InputManager : MonoBehaviour {
     int line_leftgrip = 1;
     int line_rightgrip = 1;
 
-<<<<<<< HEAD
     static InputManager instance;
 
     public static InputManager GetInstance()
@@ -84,15 +59,6 @@ public class InputManager : MonoBehaviour {
         sensor = new BmcmSensor("usb-ad");
         sensor.Init();
 
-=======
-    private void Awake()
-    {
-        Configuration.LoadConfig();
-
-        sensor = new BmcmSensor("usb-ad");
-        sensor.Init();
-
->>>>>>> e4d8a318bd281d19399a63c807e2594eeae29315
     }
 
     // Use this for initialization
@@ -115,17 +81,11 @@ public class InputManager : MonoBehaviour {
         minRawVal_ripcord = (float)Configuration.GetInnerTextByTagName("minRawVal_ripcord", minRawVal_ripcord);
         maxRawVal_ripcord = (float)Configuration.GetInnerTextByTagName("maxRawVal_ripcord", maxRawVal_ripcord);
 
-        threshold_move = (float)Configuration.GetInnerTextByTagName("threshold_move", threshold_move);
-        threshold_pull = (float)Configuration.GetInnerTextByTagName("threshold_pull", threshold_pull);
-
-<<<<<<< HEAD
         deadzone            = (float)Configuration.GetInnerTextByTagName("deadzone", deadzone);
         threshold_pull       = (float)Configuration.GetInnerTextByTagName("threshold_pull", threshold_pull);
         threshold_control   = (float)Configuration.GetInnerTextByTagName("threshold_control", threshold_control);
         threshold_idle = (float)Configuration.GetInnerTextByTagName("threshold_idle", threshold_idle);
 
-=======
->>>>>>> e4d8a318bd281d19399a63c807e2594eeae29315
         enableKeyboard = Configuration.GetInnerTextByTagName("debug", false);
 
         Cursor.visible = false;
@@ -151,7 +111,6 @@ public class InputManager : MonoBehaviour {
             Application.Quit();
         }
 
-<<<<<<< HEAD
         if (sensor.IsValid())
         {
             float rawVal_ripcord    = sensor.GetAnalogIn(port_rightGrip, line_ripcord);
@@ -217,68 +176,6 @@ public class InputManager : MonoBehaviour {
         }
     }
 
-=======
-        if (sensor.IsValid())
-        {
-            float rawVal_ripcord    = sensor.GetAnalogIn(port_rightGrip, line_ripcord);
-            float rawVal_left       = sensor.GetAnalogIn(port_leftGrip, line_leftgrip);
-            float rawVal_right      = sensor.GetAnalogIn(port_rightGrip, line_rightgrip);
-
-            if (Input.GetKey(KeyCode.C))
-            {
-                if (Input.GetKeyDown(KeyCode.S))
-                {
-                    minRawValue_leftgrip = rawVal_left;
-                    minRawValue_rightgrip = rawVal_right;
-                    minRawVal_ripcord = rawVal_ripcord;
-
-                    Configuration.SaveValueInConfig(minRawValue_leftgrip.ToString(), "minRawValue_leftgrip");
-                    Configuration.SaveValueInConfig(minRawValue_rightgrip.ToString(), "minRawValue_rightgrip");
-                    Configuration.SaveValueInConfig(minRawVal_ripcord.ToString(), "minRawVal_ripcord");
-                }
-                else if (Input.GetKeyDown(KeyCode.E))
-                {
-                    maxRawValue_leftgrip = rawVal_left;
-                    maxRawValue_rightgrip = rawVal_right;
-
-                    Configuration.SaveValueInConfig(maxRawValue_leftgrip.ToString(), "maxRawValue_leftgrip");
-                    Configuration.SaveValueInConfig(maxRawValue_rightgrip.ToString(), "maxRawValue_rightgrip");
-                }
-                else if (Input.GetKeyDown(KeyCode.P))
-                {
-                    maxRawVal_ripcord = rawVal_ripcord;
-                    Configuration.SaveValueInConfig(maxRawVal_ripcord.ToString(), "maxRawVal_ripcord");
-                }
-            }
-
-            //normalized Values
-            normalizedVal_left = Remap(rawVal_left, minRawValue_leftgrip, maxRawValue_leftgrip, -1, 1);
-            normalizedVal_right = Remap(rawVal_right, minRawValue_rightgrip, maxRawValue_rightgrip, -1, 1);
-            normalizedVal_ripcord = Remap(rawVal_ripcord, minRawVal_ripcord, maxRawVal_ripcord, 0, 1);
-
-            //noise value filter
-            normalizedVal_left = ReduceResolution(normalizedVal_left, oldNormalizedVal_left);
-            normalizedVal_right = ReduceResolution(normalizedVal_right, oldNormalizedVal_right);
-
-            oldNormalizedVal_left = normalizedVal_left;
-            oldNormalizedVal_right = normalizedVal_right;
-        }         
-    }
-
-    float ReduceResolution(float newVal, float oldVal){
-        if (Mathf.Abs(normalizedVal_left - oldNormalizedVal_left) < threshold_move)
-        {
-            return oldVal;
-        }
-        return newVal;
-     }
-
-    float Remap(float _val, float _minIn, float _maxIn, float _minOut, float _maxOut)
-    {
-        return _minOut + (_maxOut - _minOut) * (_val - _minIn) / (_maxIn - _minIn);
-    }
-
->>>>>>> e4d8a318bd281d19399a63c807e2594eeae29315
     void OnGUI()
     {
         if (showInputGUI)
@@ -288,21 +185,12 @@ public class InputManager : MonoBehaviour {
             int y = 380;
 
             GUI.Box(new Rect(10, y, 200, 200), "");
-<<<<<<< HEAD
             GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "USB-AD is " + sensor.IsValid());
             GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "raw Value left: " + rawVal_left);
             GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "norm Value left: " + normalizedVal_leftGrip);
             GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "raw Value right: " + rawVal_right);
             GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "norm Value right: " + normalizedVal_rightGrip);
             GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "norm Value ripcord: " + normalizedVal_ripcord);
-=======
-            GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "USB-AD is "            + sensor.IsValid() );
-            GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "raw Value left: "      + rawVal_left);
-            GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "norm Value left: "     + normalizedVal_left);
-            GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "raw Value right: "     + rawVal_right);
-            GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "norm Value right: "    + normalizedVal_right);
-            GUI.Label(new Rect(x, y + (++yPos) * lineHeight, 500, 500), "norm Value ripcord: "  + normalizedVal_ripcord);
->>>>>>> e4d8a318bd281d19399a63c807e2594eeae29315
         }
     }
 
@@ -334,7 +222,6 @@ public class InputManager : MonoBehaviour {
     }
 
     // move to left or right
-<<<<<<< HEAD
     public float GetResultLeftRightMinus1To1()
     {
         if (enableKeyboard)
@@ -369,42 +256,6 @@ public class InputManager : MonoBehaviour {
         }
 
         return false;
-=======
-    public float GetHorinatalAxes()
-    {
-        if (enableKeyboard)
-        {
-            return Input.GetAxis("Horizontal");
-        }
-
-        return (normalizedVal_left - normalizedVal_right) * 0.5f;
-    }
-
-    // move to up or down
-    public float GetVerticalAxes()
-    {
-        if (enableKeyboard)
-        {
-            return Input.GetAxis("Vertical");
-        }
-
-        return (normalizedVal_left + normalizedVal_right) * 0.5f;
-    }
-
-    public bool PullRipcord()
-    {
-        if (enableKeyboard)
-        {
-            return Input.GetKeyDown(KeyCode.Space);
-        }
-
-        if (normalizedVal_ripcord > threshold_pull)
-        {
-            return true;
-        }
-
-        return false;
->>>>>>> e4d8a318bd281d19399a63c807e2594eeae29315
     }
 
     public bool ControlledGrips()
