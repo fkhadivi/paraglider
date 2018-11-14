078022415dfb2b65097f506b64f1faceb1aace1d
benjamin kiesewetter <birk.borkasson@gmx.de>
2018-11-14 20:19:38 +0100

    Buglist fixing
    billboard Y axis only (frame, trees, obstackles)
    single level loading
    update map position on start
    slide along house fassades
    colliders for max height
    start position city higher

---
+++ paraglider unity/Assets/scripts/crashTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crashTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        rigi = GetComponent<Rigidbody>();
+        
+	}
+
+    public Rigidbody rigi;
+
+    // Update is called once per frame
+    void Update () {
+        Debug.DrawLine(transform.position, transform.position + rigi.velocity, Color.magenta);
+        veloBackup = velo;
+        velo = rigi.velocity;
+
+    }
+
+    public float crashAngle(Collision collision)
+    {
+        return Vector3.Angle(collision.relativeVelocity, collision.contacts[0].normal);
+    }
+
+    public float angle;
+    public Vector3 velo;
+    public Vector3 veloBackup;
+    public Vector3 veloOnCrash;
+    public Vector3 veloRel;
+
+    public Vector3 normal;
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        normal = collision.contacts[0].normal;
+        angle = crashAngle(collision);
+        veloOnCrash = rigi.velocity;
+        veloRel = collision.relativeVelocity;
+        Debug.DrawLine(transform.position, transform.position + collision.contacts[0].normal.normalized*999, Color.green);
+        Debug.DrawLine(transform.position, transform.position + collision.relativeVelocity * 999, Color.blue);
+        Debug.Break();
+    }
+}
